2000c8775082fbe59421eac6c944f7ae035eeb6f
mkmelhundley-code <mkmelhundley@gmail.com>
2026-04-16 12:26:53 -0700

    updated font settings for main page w/no entries and added a basic footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ export default function App() {
   return (
     <div className="app-container">
       <PrayerJournal />
+      <div className="footer">
+        <p>
+          This app is built by{" "}
+          <a
+            href="https://www.mjhdesignstudio.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MJH Design Studio
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
